6b68a5eb09a56da7fcd3961abf119ea1c6cdb0d4
nazpedawi <144240165+nazpedawi@users.noreply.github.com>
2025-05-15 13:40:42 +0200

    accounts list improved

--- src/views/AccountsView.vue
+++ src/views/AccountsView.vue
@@ -3,7 +3,7 @@ import AccountsList from "../components/AccountsList.vue";
 </script>
 
 <template>
-  <main>
+  <main class="container-fluid d-flex flex-column flex-grow-1 align-items-center p-0">
     <AccountsList/>
   </main>
 </template>
