8314c158f982579524a34b671db8b34747bb0a09
ZhadykDanylo <zhadykdanylo@gmail.com>
2025-05-27 14:20:01 +0200

    separated account creation from approval logic

--- src/utils/config.js
+++ src/utils/config.js
@@ -11,9 +11,10 @@ export const API_ENDPOINTS = {
   accountsById: (id) => `${API_BASE_URL}/users/${id}/accounts`,
   accounts: `${API_BASE_URL}/accounts`,
   usersPending: `${API_BASE_URL}/users/pending`,
-  usersApproval: (id) => `${API_BASE_URL}/users/${id}/approval`,
+  usersApproval: (id) => `${API_BASE_URL}/users/${id}/approval-status`,
   usersById: (id) => `${API_BASE_URL}/users/${id}`,
   accountsByIdReview: (id) => `${API_BASE_URL}/users/${id}/accounts/review`,
   updateLimits: (iban) => `${API_BASE_URL}/accounts/${iban}/limits`,
-  accountsByName: (firstName, lastName) => `${API_BASE_URL}/users/accounts/${firstName}/${lastName}`
+  accountsByName: (firstName, lastName) => `${API_BASE_URL}/users/accounts/${firstName}/${lastName}`,
+  userAccounts: (id) => `${API_BASE_URL}/users/${id}/accounts`,
 };
